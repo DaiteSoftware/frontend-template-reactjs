608953ca7604484cc9441e422e0419b5828039ad
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-10-31 16:51:36 -0400

    refactor: remove Navbar component from Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import useAuth from "../hooks/useAuth";
-import { Navbar } from "../components/Navbar";
+// import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
   const { auth } = useAuth({});
 
   return (
     <div>
-      <Navbar />
       <h1>Bienvenido {auth?.user?.usuario}</h1>
     </div>
-  )
-}
+  );
+};
