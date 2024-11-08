1c766b9b7c4548dff7185c8e5dbf12f28cc878ca
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-11-08 14:26:19 -0500

    refactor: update Layout component for improved responsiveness and structure

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -3,10 +3,10 @@ import { Navbar } from "../Navbar";
 
 export default function Layout() {
   return (
-    <div className="bg-neutral-100 h-screen w-screen overflow-hidden flex flex-row">
+    <div className="bg-neutral-100 min-h-screen flex flex-col md:flex-row">
       <div className="flex flex-col flex-1">
         <Navbar />
-        <div className="flex-1  min-h-0 overflow-auto">
+        <div className="flex-1 min-h-screen overflow-auto">
           <Outlet />
         </div>
       </div>
