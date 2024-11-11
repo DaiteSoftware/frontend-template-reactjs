e2a92d5cdccf5eb2b95065f17630975809854271
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-11-11 15:08:46 -0500

    feat: update Tailwind CSS configuration with new color variables for improved styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./app.jsx",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app.jsx", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#fafaf9",       
-        secondary: "#d97706", 
+        primary: "#fafaf9",
+        secondary: "#d97706",
         hoverColor: "#f59e0b",
         colorText: "#3f3f46",
         borderColor: "#d4d4d8",
-        textHoverColor: "#fafafa"
+        textHoverColor: "#fafafa",
+        mainTableColor: "#2563eb",
+        focusColor: "#1d4ed8",
+        zebraColor: "#dbeafe",
       },
     },
   },
